000358e20866791efb8c69eec57dbf66ff186b19
Saumya Jaiswal <24jaiswalsaumya@gmail.com>
2023-04-03 23:03:18 +0530

    loop in react

--- src/App.js
+++ src/App.js
@@ -2,19 +2,33 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
 
   const expenses = [
-    {title:'Car Expense', amount:294.47, date : new Date (2021,2,28)},
-    {title:'Flat Expense', amount:294.47, date : new Date (2021,4,28)},
-    {title:'Food Expense', amount:294.47, date : new Date (2021,6,28)},
-    {title:'Electricity Expense', amount:294.47, date : new Date (2021,8,28)},
-  ]
+    {title:'Car Expense', amount:294.47, date : new Date (2021,2,28), location :'Noida'},
+    {title:'Flat Expense', amount:294.47, date : new Date (2021,4,28),location :'Delhi'},
+    {title:'Food Expense', amount:294.47, date : new Date (2021,6,28),location :'Greater Noida'},
+    {title:'Electricity Expense', amount:294.47, date : new Date (2021,8,28),location :'Delhi'},
+  ];
+
+  var data = []
+
+  for(var i=0 ; i<expenses.length; i++){
+    data.push(<ExpenseItem title={expenses[i].title} amount={expenses[i].amount} date={expenses[i].date} location={expenses[i].location}></ExpenseItem>)
+  }
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+
+      <div>{data}</div>
+      
+      
+      {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} location={expenses[0].location}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} location={expenses[1].location}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} location={expenses[2].location}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} location={expenses[3].location}></ExpenseItem> */}
+    
     </div>
+
+
     
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
 import './ExpenseItem.css'
 function ExpenseItem(props){
    
-    const expenseLocation = 'Noida';
+    
     return (
     <div className="expense-item">
         <div>{props.date.toISOString()}</div>
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
-            <h2>{expenseLocation}</h2>
+            <h2>{props.location}</h2>
         </div>
     </div>
     
