d977e896246a79598ed11329d1b4568951369b60
Saumya Jaiswal <24jaiswalsaumya@gmail.com>
2023-04-07 13:26:04 +0530

    Events

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,11 +3,23 @@ import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card'
 import './ExpenseItem.css'
 const ExpenseItem = (props) => {
+
+    const clickHandler = ()=>{
+        console.log('Clicked!!!')
+    }
+
+    const deleteExpense=()=>{
+        var e=document.getElementsByClassName('abc')
+        e.parentNode.removeChild(e)
+    
+    }
     
     return (
     <Card className='expense-item'>
-        <ExpenseDate date={props.date}></ExpenseDate>
+        <ExpenseDate date={props.date} className='abc'></ExpenseDate>
         <ExpenseDetails title={props.title} amount={props.amount} location={props.location}></ExpenseDetails>
+        <button onClick={clickHandler}>Change title</button>
+        <button onClick={()=>deleteExpense()}>Delete</button>
     </Card>
     
     );
